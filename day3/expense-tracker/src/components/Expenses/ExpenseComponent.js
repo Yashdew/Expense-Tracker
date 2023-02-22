c4109b6f2d0c36f6b0039529c223e3ce6e9576fe
Yashdew <yashdewangan123456@gmail.com>
2023-02-22 14:46:02 +0530

    Expenses in List form

--- day3/expense-tracker/src/components/Expenses/ExpenseComponent.js
+++ day3/expense-tracker/src/components/Expenses/ExpenseComponent.js
@@ -1,24 +1,74 @@
-import ExpenseItem from "./ExpenseItem";
 import "./ExpenseComponent.css";
 import Cards from "../UI/Cards";
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
+import ExpenseList from "./ExpenseList";
 
 const ExpenseComponents = (props) => {
   
-  const [filterYear,setFilterYear] = useState('2022');
+  const [filterYear,setFilterYear] = useState('2023');
 
   const filterYearChangehandler = (selectedYear) => {
+      //console.log(selectedYear);
       setFilterYear(selectedYear);
   }
-  
+
+  const filteredExpenses = props.expenses.filter(expense => {
+    return expense.date.getFullYear().toString() === filterYear;
+  });
+
+  // let expenseContent = (<p>No expense found</p>);
+
+  // if(filteredExpenses > 0){
+  //       expenseContent = filteredExpenses.map( (expense,index) => <ExpenseItem 
+  //       key={expense.id}
+  //       title={expense.title}
+  //       amount={expense.amount}
+  //       date={expense.date}
+  //     />
+  //   )
+  // }
   return (
     <div>
         
         {/* Add filter Components */}
         <Cards className="expenses">
         <ExpensesFilter selected = {filterYear} onChangeFilter = {filterYearChangehandler}/>
-        <ExpenseItem
+        
+        {/* {expenseContent} */}
+        {/* { filteredExpenses.length === 0 && <p>No expense found</p>}
+        { filteredExpenses.length > 0 && (filteredExpenses.map( (expense,index) => <ExpenseItem 
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+        />) ) } */}
+        <ExpenseList items={filteredExpenses}/>
+  
+        {/* {filteredExpenses.length === 0 ? 
+          (<p>No expense found</p> )
+          : 
+          (
+              filteredExpenses.map( (expense,index) => <ExpenseItem 
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+              />
+          ))
+        } */}
+        
+        {/* filter data check filteredExpense function() */}
+        {/* {props.expenses.map( (expense,index) => <ExpenseItem 
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+          />
+        )} */}
+        {/* ALL data can be rendered using props */}
+        
+        {/* <ExpenseItem
           title={props.expenses[0].title}
           amount={props.expenses[0].amount}
           date={props.expenses[0].date}
@@ -37,7 +87,7 @@ const ExpenseComponents = (props) => {
           title={props.expenses[3].title}
           amount={props.expenses[3].amount}
           date={props.expenses[3].date}
-        ></ExpenseItem>
+        ></ExpenseItem> */}
       </Cards>
     </div>
     
